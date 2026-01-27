1fa7ed002ff1bfb39490d3b5928be2d5e90392d9
taslim19 <ataslimx@gmail.com>
2026-01-27 17:32:59 +0530

    chore: add Docker deployment config for Northflank

--- next.config.js
+++ next.config.js
@@ -1,11 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    output: 'standalone', // Required for Docker deployment
     images: {
         remotePatterns: [
             {
                 protocol: 'https',
                 hostname: 'api.telegram.org',
+            },
+            {
+                protocol: 'https',
+                hostname: 'cdn.huggingface.co',
             },
         ],
     },
